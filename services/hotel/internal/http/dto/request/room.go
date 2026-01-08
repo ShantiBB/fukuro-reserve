f3c136fcfa010eaab48710ea35ae146e806fbd94
segei.golobokov <segeygolobokov@gmail.com>
2026-01-08 22:45:55 +0300

    Fix validation fields and room db queries

--- services/hotel/internal/http/dto/request/room.go
+++ services/hotel/internal/http/dto/request/room.go
@@ -7,27 +7,27 @@ import (
 )
 
 type RoomCreate struct {
-	Title       string          `json:"title" validate:"required"`
-	Description *string         `json:"description"`
-	RoomNumber  string          `json:"room_number" validate:"required"`
-	Type        models.RoomType `json:"type" validate:"required"`
-	Price       decimal.Decimal `json:"price" validate:"required"`
-	Capacity    int             `json:"capacity" validate:"required"`
-	AreaSqm     float64         `json:"area_sqm" validate:"required"`
-	Floor       int             `json:"floor" validate:"required"`
-	Amenities   []string        `json:"amenities"`
-	Images      []string        `json:"images"`
+	Title       string           `json:"title" validate:"required"`
+	Description *string          `json:"description"`
+	RoomNumber  string           `json:"room_number" validate:"required"`
+	Type        models.RoomType  `json:"type" validate:"required"`
+	Price       *decimal.Decimal `json:"price" validate:"required"`
+	Capacity    int              `json:"capacity" validate:"required"`
+	AreaSqm     float64          `json:"area_sqm" validate:"required"`
+	Floor       int              `json:"floor" validate:"required"`
+	Amenities   []string         `json:"amenities"`
+	Images      []string         `json:"images"`
 }
 
 type RoomUpdate struct {
-	Title       string          `json:"title" validate:"required"`
-	Description *string         `json:"description"`
-	RoomNumber  string          `json:"room_number" validate:"required"`
-	Type        models.RoomType `json:"type" validate:"required"`
-	Price       decimal.Decimal `json:"price" validate:"required"`
-	Capacity    int             `json:"capacity" validate:"required"`
-	AreaSqm     float64         `json:"area_sqm" validate:"required"`
-	Floor       int             `json:"floor" validate:"required"`
-	Amenities   []string        `json:"amenities"`
-	Images      []string        `json:"images"`
+	Title       string           `json:"title" validate:"required"`
+	Description *string          `json:"description"`
+	RoomNumber  string           `json:"room_number" validate:"required"`
+	Type        models.RoomType  `json:"type" validate:"required"`
+	Price       *decimal.Decimal `json:"price" validate:"required"`
+	Capacity    int              `json:"capacity" validate:"required"`
+	AreaSqm     float64          `json:"area_sqm" validate:"required"`
+	Floor       int              `json:"floor" validate:"required"`
+	Amenities   []string         `json:"amenities"`
+	Images      []string         `json:"images"`
 }
